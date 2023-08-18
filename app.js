8f697fffcded9600fc557f858dd7a78ac829c05f
limlauer <psimlauer@gmail.com>
2023-08-18 11:57:55 -0300

    main

---
+++ app.js
@@ -0,0 +1,36 @@
+var canvas = document.getElementById("drawing-board");
+var ctx = canvas.getContext("2d");
+
+var model = tf.loadModel("model.h5");
+
+var drawing = [];
+
+function draw(event) {
+    drawing.push(event.x, event.y);
+    ctx.fillStyle = "black";
+    ctx.fillRect(event.x, event.y, 1, 1);
+}
+
+canvas.addEventListener("mousedown", draw);
+
+function recognize() {
+    var image = tf.image.arrayToTensor(drawing);
+    image = tf.expandDims(image, 0);
+    var prediction = model.predict(image);
+    document.getElementById("prediction").innerHTML = prediction[0];
+}
+
+document.getElementById("erase-button").addEventListener("click", function() {
+    drawing = [];
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+document.getElementById("recognize-button").addEventListener("click", recognize);
+
+document.getElementById("correct-button").addEventListener("click", function() {
+    document.getElementById("prediction").innerHTML = "Correct";
+});
+
+document.getElementById("incorrect-button").addEventListener("click", function() {
+    document.getElementById("prediction").innerHTML = "Incorrect";
+});
